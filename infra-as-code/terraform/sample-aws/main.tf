3af81d33984c8964e65552d89a7dce92053fa2cc
sudarshanswati <sudarshan41@gmail.com>
2024-08-21 22:57:17 +0530

    Update main.tf

--- infra-as-code/terraform/sample-aws/main.tf
+++ infra-as-code/terraform/sample-aws/main.tf
@@ -50,9 +50,7 @@ data "tls_certificate" "thumb" {
 }
 
 provider "kubernetes" {
-  host                   = "${data.aws_eks_cluster.cluster.endpoint}"
-  cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
-  token                  = "${data.aws_eks_cluster_auth.cluster.token}"
+  config_path = "~/.kube/config"
 }
 
 module "eks" {
